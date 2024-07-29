f00087fb667f8e9edb4ae1a398cfb96057ad1b95
tiaagustiana <tiaagustiana583@gmail.com>
2024-07-29 10:56:52 +0700

    update git

--- assets/js/main.js
+++ assets/js/main.js
@@ -236,3 +236,10 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 })();
+
+window.onload = function() { 
+  var el = document.getElementById('g-recaptcha-response'); 
+  if (el) { 
+    el.setAttribute('required', 'required'); 
+  } 
+}
