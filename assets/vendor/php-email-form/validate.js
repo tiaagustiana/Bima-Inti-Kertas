873a40bbe3035f7512edb452f38b4ea0b420d3bd
tiaagustiana <tiaagustiana583@gmail.com>
2024-07-29 15:06:00 +0700

    update

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -196,5 +196,13 @@ window.onload = function() {
   var el = document.getElementById('g-recaptcha-response'); 
   if (el) { 
     el.setAttribute('required', 'required'); 
-  } 
+  }
+  
+  document.querySelector('.php-email-form').addEventListener('submit', function(event) {
+    var recaptchaResponse = grecaptcha.getResponse();
+    if (!recaptchaResponse) {
+      event.preventDefault();
+      alert('Silahkan lengkapi reCAPTCHA');
+    }
+  })
 }
